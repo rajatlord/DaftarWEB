862b58cf6d455a66d4fa25704f8015fbb67a5764
rajat mathur <rajatmathur1881@gmail.com>
2025-11-10 09:54:22 +0530

    fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import phone from "../../../public/assets/footer/phone.png"
 import email from "../../../public/assets/footer/email.png"
 import clock from "../../../public/assets/footer/clock.png"
-import vocal from "../../../public/assets/footer/Vocal.png"
+import vocal from "../../../public/assets/footer/vocal.png"
 
 import playstore from "../../../public/assets/footer/playstore.png"
 import payments from "../../../public/assets/footer/payments.png"
